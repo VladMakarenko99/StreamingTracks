ee3d53af6611917b088e45d4cb3b093b7b7cfea7
VladMakarenko99 <goze23gooz@gmail.com>
2024-11-21 15:57:19 +0100

    Update Program.cs

--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -14,11 +14,10 @@ builder.Services
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseHttpsRedirection();
 
