faa0a1b18c0dfe44591d57617d849223d8b0240a
VladMakarenko99 <goze23gooz@gmail.com>
2024-12-27 15:54:43 +0100

    Made navigation to be looped

--- src/Infrastructure/Repository/SoundtrackRepository.cs
+++ src/Infrastructure/Repository/SoundtrackRepository.cs
@@ -27,7 +27,7 @@ public class SoundtrackRepository(AppDbContext context) : ISoundtrackRepository
         context.Remove(soundtrack);
         await context.SaveChangesAsync();
     }
-    
+
     public async Task<(Guid? PrevTrackId, Guid? NextTrackId)> GetPreviousAndNextTrackIds(Soundtrack currentTrack)
     {
         var prevTrackId = await context.Soundtracks
@@ -35,14 +35,29 @@ public class SoundtrackRepository(AppDbContext context) : ISoundtrackRepository
             .OrderByDescending(t => t.CreatedAt)
             .Select(t => t.Id)
             .FirstOrDefaultAsync();
-
+        
+        if (prevTrackId == Guid.Empty)
+        {
+            prevTrackId = await context.Soundtracks
+                .OrderBy(t => t.CreatedAt)
+                .Select(t => t.Id)
+                .LastOrDefaultAsync();
+        }
+        
         var nextTrackId = await context.Soundtracks
             .Where(t => t.CreatedAt > currentTrack.CreatedAt)
             .OrderBy(t => t.CreatedAt)
             .Select(t => t.Id)
             .FirstOrDefaultAsync();
+        
+        if (nextTrackId == Guid.Empty)
+        {
+            nextTrackId = await context.Soundtracks
+                .OrderBy(t => t.CreatedAt)
+                .Select(t => t.Id)
+                .FirstOrDefaultAsync();
+        }
 
-        return (prevTrackId == Guid.Empty ? (Guid?)null : prevTrackId,
-            nextTrackId == Guid.Empty ? (Guid?)null : nextTrackId);
+        return (prevTrackId, nextTrackId);
     }
 }
