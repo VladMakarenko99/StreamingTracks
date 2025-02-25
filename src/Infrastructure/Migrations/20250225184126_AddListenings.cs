d7534eb0d4d7d8ad989ef98bdd11a72e614665cd
VladMakarenko99 <goze23gooz@gmail.com>
2025-02-25 20:48:26 +0100

    Pushed migrations

---
+++ src/Infrastructure/Migrations/20250225184126_AddListenings.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddListenings : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Listenings",
+                table: "Soundtracks",
+                type: "integer",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Listenings",
+                table: "Soundtracks");
+        }
+    }
+}
