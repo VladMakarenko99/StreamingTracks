7e77a72aa4afb7f2e7b3ae245f64f432d524a8f5
VladMakarenko99 <goze23gooz@gmail.com>
2025-02-26 19:18:11 +0100

    Removed unnecessary elements of application

--- src/Application/Streaming/Queries/Stream/StreamCommand.cs
+++
@@ -1,7 +0,0 @@
-using Application.DTOs.Result;
-using MediatR;
-using Microsoft.AspNetCore.Mvc;
-
-namespace Application.Streaming.Queries.Stream;
-
-public record StreamCommand(Guid Id) : IRequest<Result<FileStreamResult>>;
